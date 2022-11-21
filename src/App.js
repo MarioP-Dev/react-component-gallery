d244a78ebdc7737e50288c2e9a2ac220c866d9d6
Mario Pérez (MacBook Pro) <mp@mariopdev.com>
2022-11-21 01:47:12 +0100

    s

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react';
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <Routes>
       <Route exact path="/" element={<h1>HOME</h1>}/>
+      <Route exact path="/test" element={<h1>test</h1>}/>
       <Route path="*" element={<h1>NO</h1>}/>
     </Routes>
   );
